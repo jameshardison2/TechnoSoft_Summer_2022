12325f822cb0de83638eabfdc252f2949d125e33
Macintosh HD <macintoshhd@Macintoshs-MacBook-Pro.local>
2022-07-01 18:01:42 -0400

    Adding Class notes

---
+++ Class1Practice/variablePractice1.js
@@ -0,0 +1,40 @@
+//1. Create a variable named accountBalance and assign 1000 to that variable.
+let accountBalance = 1000;
+
+
+//2. Let’s assume you spent $1 at the park when you went our for a walk.
+//3. Post Decrement a dollar from you accountBalance.
+accountBalance-- 
+
+
+//4. Construct a string which should display below text:
+//I spent a dollar to buy a water and not my new balance is N
+console.log(`\nI spent a dollar to buy a water and not my new balance is ${accountBalance-- }.`)
+
+console.log(`\n\n\n//////////////////////////////////////////////////////////////////////////////////////\n\n\n`)
+
+//1. Create a variable name age and assign your age to that variable
+let age = 40;
+
+
+//2. Lets assume today is your birthday 
+let myBirthday = `July 28th`;
+
+//3. Post Increment your age
+age++
+
+//4. Construct a string which should display below text
+console.log(`Since today is ${myBirthday}, I am ${age} years of age.`);
+
+console.log(`\n\n\n//////////////////////////////////////////////////////////////////////////////////////\n\n\n`)
+
+
+var variables = require('metasyntactic-variables');
+console.log(variables.join('\n'));
+
+
+console.log(variables);
+
+
+
+
