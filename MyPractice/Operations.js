fc602b7352b2bd31cac0c57f2681187b101dfd89
James Hardison II <macintoshhd@Macintoshs-MacBook-Pro.local>
2022-07-02 21:09:53 -0400

    JS Code update

---
+++ MyPractice/Operations.js
@@ -0,0 +1,33 @@
+/**
+ * ternary operators:
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 4.5, 5.5
+ * 
+ * If credit score is greater than 700, rate will be 4.5 else 5.5
+ * 
+ * 
+ * If credit score is greater than 700 and first time home buyers, rate will be 4.5 else 5.5
+ * 
+ *  * If credit score or greater than 700 and first time home buyers, rate will be 4.5 else 5.5
+ */
+
+
+ //If credit score is greater than 700, rate will be 4.5 else 5.5
+let userCreditScore  = 770;
+
+let mortgageRate = userCreditScore > 700 ? 4.5 : 5.5;
+
+console.log(`\nCredit Score: ${userCreditScore}`);
+console.log(`\nMortgage Rate: ${mortgageRate}`);
+
+
+
+
+
+  
+
